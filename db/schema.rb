6af4c87787e03b0ee8a1528280da76b2c0fe273f
aaron foster bresley <afbresley@gmail.com>
2016-09-12 22:20:49 -0400

    add picture model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902022145) do
+ActiveRecord::Schema.define(version: 20160913014241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 20160902022145) do
     t.datetime "date"
     t.string   "location"
     t.string   "price_line",    default: ""
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "avatar"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "projects", force: :cascade do |t|
